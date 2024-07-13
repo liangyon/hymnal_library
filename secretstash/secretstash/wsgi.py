d91229e6a683d4b10faf22b7ebb1cb0c7ee3a621
Brandon Liang <Brandon.Liang3@ontario.a>
2024-07-13 01:00:14 -0700

    added more remote rules

--- secretstash/secretstash/wsgi.py
+++ secretstash/secretstash/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'secretstash.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'secretstash.secretstash.settings')
 
 application = get_wsgi_application()
