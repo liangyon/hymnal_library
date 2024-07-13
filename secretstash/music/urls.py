74633ed99adf7009597cac00fea395fa180e7a28
Brandon Liang <Brandon.Liang3@ontario.a>
2024-07-13 01:22:03 -0700

    added more remote rules

--- secretstash/music/urls.py
+++ secretstash/music/urls.py
@@ -1,11 +1,12 @@
 # music/urls.py
 from django.urls import path
 from django.contrib.auth import views as auth_views
+from django.contrib.auth.decorators import login_required
 from . import views
 
 urlpatterns = [
     path('upload/sheet_music/', views.upload_sheet_music, name='upload_sheet_music'),
-    path('upload/multiple_sheet_music/', views.FileFieldFormView.as_view(), name='upload_multiple_sheet_music'),
+    path('upload/multiple_sheet_music/', login_required(views.FileFieldFormView.as_view()), name='upload_multiple_sheet_music'),
     path('sheets/<int:sheetID>/', views.music_page, name='music_page'),
     path('accounts/login/',
          auth_views.LoginView.as_view(template_name='music/registration/login.html'),
